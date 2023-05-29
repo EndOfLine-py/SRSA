a3d2c03478aea1395d2c21523b15f1680683ba06
End <54771755+EndOfLine-py@users.noreply.github.com>
2023-05-29 16:02:54 +0200

    Update SRSA.cpp

--- SRSA.cpp
+++ SRSA.cpp
@@ -20,10 +20,19 @@ int LDR2_val;
 int LDR3_val;
 int LDR4_val;
 // stockage de la dernière valeur
-int LDR1_lastval;
-int LDR2_lastval;
-int LDR3_lastval;
-int LDR4_lastval;
+//int lastval1;
+//int lastval2;
+//int lastval3;
+//int lastval4;
+//int LDR1_lastval;
+//int LDR2_lastval;
+//int LDR3_lastval;
+//int LDR4_lastval;
+
+// checks
+bool check1; // pour verif les conditions et tout mgl
+bool check2;
+
 
 // servos n shit
 Servo Base;
@@ -52,5 +61,61 @@ void setup() {
 
 // la boucle
 void loop() {
+    int LDR1_val = analogRead(LDR1_pin);
+    int LDR2_val = analogRead(LDR2_pin);
 
+
+    int value1 = abs(LDR1_val - LDR2_val);
+    int value2 = abs(LDR2_val - LDR1_val);
+
+
+    if ((value1 <= tol) || (value2 <= tol)) {
+        check1 = true;;
+    }
+    else {
+        check1 = false;
+        if (LDR1_val > LDR2_val) {
+            Base_pos = --Base_pos;
+        }
+
+        if (LDR1_val < LDR2_val) {
+            Base_pos = ++Base_pos;
+        }
+    }
+
+    Base.write(Base_pos);
+
+    /////////////////////////////////////////
+
+    int LDR3_val = analogRead(LDR3_pin);
+    int LDR4_val = analogRead(LDR4_pin);
+
+
+    int value3 = abs(LDR3_val - LDR4_val);
+    int value4 = abs(LDR4_val - LDR3_val);
+
+
+    if ((value3 <= tol) || (value4 <= tol)) {
+        check2 = true;
+    }
+    else {
+        check2 = false;
+        if (LDR3_val > LDR4_val) {
+            Tour_pos = --Tour_pos;
+        }
+
+        if (LDR3_val < LDR4_val) {
+            Tour_pos = ++Tour_pos;
+        }
+    }
+
+    Tour.write(Tour_pos);
+    
+    if ((check1 == true) && (check2 == true)) {
+        delay(6000);
+    }
+    else {
+        delay(80);
+    }
+    
 }
